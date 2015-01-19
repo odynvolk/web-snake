bb915225ee648bab3738e8828adcf92c6bddcc54
Alexi Rahman <alexi.rahman@r76.se>
2015-01-19 23:41:50 +0100

    Added a couple of more tests and using vcrpy for recording the requests

--- tests/test_web_snake.py
+++ tests/test_web_snake.py
@@ -1,4 +1,5 @@
 import unittest
+import vcr
 from web_snake import WebSnake
 
 
@@ -10,5 +11,14 @@ class TestWebSnake(unittest.TestCase):
         self.assertEqual(self.web_snake.clean('http://www.google.com|2'), 'http://www.google.com')
         self.assertEqual(self.web_snake.clean('http://www.google.com#dfgdfg'), 'http://www.google.com')
 
+    @vcr.use_cassette('fixtures/vcr_cassettes/crawl_max_level_1.yaml')
     def test_crawl_with_max_level_1(self):
         self.assertTrue(len(self.web_snake.crawl('http://dppiwapikalipasir.org/halkomentar-153-1469.html', 1)) >= 2)
+
+    @vcr.use_cassette('fixtures/vcr_cassettes/crawl_max_level_2.yaml')
+    def test_crawl_with_max_level_1(self):
+        self.assertTrue(len(self.web_snake.crawl('http://dppiwapikalipasir.org/halkomentar-153-1469.html', 2)) >= 100)
+
+    @vcr.use_cassette('fixtures/vcr_cassettes/crawl_max_level_3.yaml')
+    def test_crawl_with_max_level_1(self):
+        self.assertTrue(len(self.web_snake.crawl('http://dppiwapikalipasir.org/halkomentar-153-1469.html', 3)) >= 1000)
